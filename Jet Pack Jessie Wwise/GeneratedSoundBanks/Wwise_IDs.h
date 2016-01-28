ad43c4170fdca387b9ed0aba3eec58e619a2243e
Dicky Moore <dickymoore@gmail.com>
2016-01-28 15:34:11 +0000

    File management

---
+++ Jet Pack Jessie Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CUBE_SOUND_EMITTER = 2147762097U;
+        static const AkUniqueID START_MUSIC_EVENT = 1857450910U;
+        static const AkUniqueID STOP_MUSIC_EVENT = 3935460844U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID JESSIE = 2992761690U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
